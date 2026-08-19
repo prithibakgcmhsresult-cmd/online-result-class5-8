ac6f7a5ddff40c3aba22f5916113140778fc84a8
prithibakgcmhsresult-cmd <prithibakgcmhsresult@gmail.com>
2026-08-19 14:55:01 +0530

    Update script.js
    1 decimal place in hth_per

--- script.js
+++ script.js
@@ -77,8 +77,8 @@ document.getElementById('searchBtn').addEventListener('click', async function() 
             <td>${d.hlth_f2a||''}</td><td>${d.hlth_f2b||''}</td><td>${d.hlth_f2c||''}</td>
             <td>${d.hlth_f3a||''}</td><td>${d.hlth_f3b||''}</td><td>${d.hlth_f3c||''}</td>
             <td>${d.hlth_s1||''}</td><td>${d.hlth_s2||''}</td><td>${d.hlth_s3||''}</td>
-            <td>${d.hlth_total||''}</td><td>${d.hlth_per ? Number(d.hlth_per).toFixed(1) : ''}</td>
-        </tr>`;
+           <td>${d.hlth_total||''}</td><td>${d.hlth_per && !isNaN(parseFloat(d.hlth_per)) ? parseFloat(d.hlth_per).toFixed(1) : ''}</td>
+         </tr>`;
         
         
         subHtml += `<tr style="background:#cfd8dc; font-weight:bold;">
